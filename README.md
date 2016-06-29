7bd7d6745e555241d8a786b92789302f9166f39c
栾博文 <lbw@terminus.io>
2016-06-29 11:19:02 +0800

    add md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # java_tools
+
+增加对于pojo对象的field空值验证
--- beanTool/src/main/java/com/hroice/beanTool/checkBeanNull/CanBeNull.java
+++ beanTool/src/main/java/com/hroice/beanTool/checkBeanNull/CanBeNull.java
@@ -3,6 +3,7 @@ package com.hroice.beanTool.checkBeanNull;
 import java.lang.annotation.*;
 
 /**
+ *
  * DATE: 16/6/28 16:17 <br>
  * MAIL: lbw@terminus.io <br>
  * AUTHOR: macbook
@@ -11,5 +12,9 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface CanBeNull {
+    /**
+     * on Class ,annotate the object's field. These field will be ignore when use CheckField.
+     * @return
+     */
     String[] value() default {""};
 }
