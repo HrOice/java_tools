2a679237f33292e6a28354a0dbce2c09edd644f4
栾博文 <lbw@terminus.io>
2016-07-21 10:22:45 +0800

    candy pointer

--- yamlExcel/src/main/java/configuration/CandyObject.java
+++ yamlExcel/src/main/java/configuration/CandyObject.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -18,23 +19,50 @@ import java.util.Map;
 public class CandyObject<T> {
     private static Class objClazz;
 
+    private T t;
+
+    private static Object obj;
+
     @Getter
     private String className;
 
     private List<CandyField<T>> candyFields;
 
-    private Object object;
+    private Iterator<CandyField<T>> fieldPoint;
 
     public void setClassName(String className) throws ClassNotFoundException{
         this.className = className;
         objClazz = Class.forName(className);
     }
 
+    public void putCandy(T t) {
+        this.t = t;
+        this.obj = this.t;
+        this.fieldPoint = candyFields.iterator();
+    }
+
+    public Object getCandy(){
+        return this.obj;
+    }
+
+    public Object pointOut(){
+        if (this.fieldPoint.hasNext()) {
+            CandyField<T> pointer = this.fieldPoint.next();
+            try {
+                return pointer.getFieldValue();
+            }catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            throw new IndexOutOfBoundsException("pointer out of " + t.getClass().getName() + "fields num");
+        }
+    }
+
     @Data
     public static class CandyField<T> {
         private String fieldName;
 
-        private Object value;
+        private String title;
 
         private Type fieldType;
 
@@ -42,11 +70,14 @@ public class CandyObject<T> {
 
         private Map enumMap;
 
-        private Object getValue() throws Exception{
-            Field field = objClazz.getField(fieldName);
+        private Object getFieldValue() throws Exception{
             Method method = objClazz.getMethod("get" + fieldName.substring(0,1).toUpperCase() + fieldName.substring(1));
-//            method.invoke()
-            return null;
+            Object value = method.invoke(obj, null);
+            if (this.enumMap != null && this.enumMap.containsKey(value)) {
+                return this.enumMap.get(value);
+            }else{
+                return value;
+            }
         }
     }
 }
---
+++ yamlExcel/src/test/java/YamlTest.java
@@ -0,0 +1,72 @@
+import bean.User;
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import configuration.CandyObject;
+import org.junit.Test;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * DATE: 16/7/20 22:31 <br>
+ * MAIL: lbw@terminus.io <br>
+ * AUTHOR: macbook
+ */
+public class YamlTest {
+    @Test
+    public void yamlTest() throws Exception{
+        String file = Resources.toString(Resources.getResource("auth/user.yaml"), Charsets.UTF_8);
+        CandyObject<User> fields = new Yaml().loadAs(file, CandyObject.class);
+        User user = new User();
+        user.setId(1L);
+        user.setName("lilei");
+        user.setStatus(2);
+        user.setType(1);
+
+        fields.putCandy(user);
+        System.out.println(fields.pointOut());
+        System.out.println(fields.pointOut());
+        System.out.println(fields.pointOut());
+        System.out.println("--");
+
+        User user1 = new User();
+        user1.setId(2L);
+        user1.setName("polly");
+        user1.setStatus(2);
+        user1.setType(1);
+        fields.putCandy(user1);
+        System.out.println(fields.pointOut());
+        System.out.println(fields.pointOut());
+        System.out.println(fields.pointOut());
+        System.out.println(fields.pointOut());
+    }
+
+    @Test
+    public void mapTest() throws Exception{
+        Map<String,String> map = new HashMap();
+        map.put("a","1");
+        map.put("b","2");
+        Yaml yaml = new Yaml();
+        FileWriter fileWriter = new FileWriter(new File("object.yml"));
+        yaml.dump(map,fileWriter);
+    }
+
+    @Test
+    public void mapParseTest() throws Exception{
+        String file = Resources.toString(Resources.getResource("auth/object.yml"), Charsets.UTF_8);
+        Map map = new Yaml().loadAs(file,Map.class);
+        System.out.println(map);
+    }
+
+    @Test
+    public void propertyTest() throws Exception{
+//        Class c = User.class;
+//        Field f = c.getDeclaredField("name");
+//        Object p = f.
+    }
+}
